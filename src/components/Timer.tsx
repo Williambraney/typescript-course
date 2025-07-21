16edb790ce1bfe66612416db1d280675ddca1260
WilliamBraney <williambraney@aol.com>
2025-07-21 16:50:31 +0100

    Working with side effects and useEffects

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,10 +1,46 @@
-import type { Timer as TimerProps } from "../store/timers-context";
+import { useEffect, useRef, useState } from "react";
+import { useTimersContext, type Timer as TimerProps } from "../store/timers-context";
 import Container from "./Container";
 
 export default function Timer({
     name,
     duration
 }: TimerProps ){
+
+    const interval = useRef<number | null>(null)
+
+    const { isRunning } = useTimersContext();
+
+    const [remainingTime, setRemainingTime] = useState(duration * 1000); // Convert seconds to milliseconds
+
+    if(remainingTime <= 0 && interval.current ) {
+        clearInterval(interval.current);
+    }
+
+    useEffect(() => {
+
+        let timer: number;
+
+        if(isRunning){
+
+        timer = setInterval(function(){
+            setRemainingTime(prev => {
+
+                if(prev <= 0) {
+                    return prev;
+                }
+                return prev - 50;
+        }); // Decrease remaining time by 50 milliseconds
+        }, 50);
+        interval.current = timer;
+
+    }else if (interval.current) {
+        clearInterval(interval.current);
+    }
+        return () => clearInterval(timer); // Clear the interval on component unmount
+    }, [isRunning]);
+
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2); // Convert milliseconds back to seconds
 
     return (
         <Container
@@ -13,8 +49,9 @@ export default function Timer({
             <h2>
                 {name}
             </h2>
+            <p><progress max = {duration * 1000} value = {remainingTime} /></p>
             <p>
-                {duration} seconds
+                {formattedRemainingTime} seconds
             </p>
         </Container>
     )
